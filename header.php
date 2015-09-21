644fceac33e0d842aa9d0d5d4b6af1fed9b0ec72
Chad Buie <chadbuie@gmail.com>
2015-09-20 22:44:37 -0400

    Updated wp-content directory!

--- header.php
+++ header.php
@@ -13,17 +13,24 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <!-- BOOTSTRAP CORE CSS -->
+    <link href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- FONT AWESOME ICONS -->
+    <link href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/font-awesome.min.css" rel="stylesheet">
+    <!-- GOOGLE FONTS -->
+    <link href="http://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet" type="text/css">
+    <!-- END BOOTSTRAP CORE CSS
 
-<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'backtaxrelief' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'backtaxrelief' ); ?></a>
     <!-- START HEADER -->
     <header>
         <!-- START MAIN NAV -->
@@ -41,14 +48,14 @@
                 </div>
 
                 <?php
-                    wp_nav_menu( array(
+                wp_nav_menu( array(
 
-                        'theme_location'  => 'primary',
-                        'container'       => 'nav',
-                        'container_class' => 'nav-collapse collapse',
-                        'menu_class'      => 'nav navbar-nav navbar-right'
+                    'theme_location'  => 'primary',
+                    'container'       => 'nav',
+                    'container_class' => 'nav-collapse collapse',
+                    'menu_class'      => 'nav navbar-nav navbar-right'
 
-                    ));
+                ));
                 ?>
                 <div id="navbar" class="collapse navbar-collapse">
                     <ul class="nav navbar-nav">
@@ -63,4 +70,4 @@
     </header>
     <!-- END HEADER -->
 
-	<div id="content" class="site-content">
+    <div id="content" class="site-content">
